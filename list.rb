e0a76984e5bd60d9753f940eb06e95bc3cc764f3
Shannon Rachal <srachal674@gmail.com>
2019-09-07 16:42:30 +0000

    Created list code

---
+++ list.rb
@@ -0,0 +1,25 @@
+class LinkedListNode
+    attr_accessor :value, :next_node
+
+    def initialize(value, next_node=nil)
+        @value = value
+        @next_node = next_node
+    end
+
+    def print_values(list_node)
+        if list_node
+            print "#{list_node.value} --> "
+            print_values(list_node.next_node)
+        else
+            print "nil\n"
+            return
+        end
+    end
+
+node1 = LinkedListNode.new(6)
+node2 = LinkedListNode.new(12, node1)
+node3 = LinkedListNode.new(74, node2)
+
+print_values(node3)
+
+end
